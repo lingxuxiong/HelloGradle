85e1a40f90eaf84d354425e8e4ce8dc9a4435335
neil <lingxuxiong@foxmail.com>
2017-03-24 16:41:43 +0800

    add toast to indicate logging status

--- app/src/main/java/com/neil/hellogradle/MainActivity.java
+++ app/src/main/java/com/neil/hellogradle/MainActivity.java
@@ -2,6 +2,7 @@ package com.neil.hellogradle;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String message = BuildConfig.enableLog ? "Logging enabled." : "Logging disabled.";
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/neil/hellogradle/MainApplication.java
+++ app/src/main/java/com/neil/hellogradle/MainApplication.java
@@ -1,9 +1,6 @@
 package com.neil.hellogradle;
 
 import android.app.Application;
-import android.util.Log;
-
-import com.neil.hellogradle.utils.Constants;
 
 /**
  * Created by nling on 3/24/2017.
@@ -13,14 +10,6 @@ public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
-        Log.d(Constants.LOG_TAG, "onCreate.");
-
-        if (BuildConfig.enableLog) {
-            Log.d(Constants.LOG_TAG, "Logging enabled.");
-        } else {
-            Log.d(Constants.LOG_TAG, "Logging disabled.");
-        }
     }
 
 }
